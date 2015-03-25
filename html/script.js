487a8271088dcdbfeb37ffe3e2f0c2bdc30781be
segur <segur.opus@gmail.com>
2015-03-25 10:43:19 +0900

    調整

--- html/script.js
+++ html/script.js
@@ -1,11 +1,13 @@
 $(function () {
-    $("#btn_play1").click(function () {
-        $("#bgsound_wav").attr("src", "voice_1.wav");
+    //btn_playをクリックした時
+    $("#btn_play").click(function () {
+        $("#audio_ie").attr("src", "voice_1.wav");                  //bgsoundタグのsrcを切り替えると音声が再生する。
+        //document.getElementById("audio_ie").src = "voice_1.wav";  //上の行と同じ処理。jQueryを利用しない表現
+        //document.all.audio_ie.src = "voice_1.wav";                //上の行と同じ処理。ただし、document.allの利用は現在非推奨
     });
-    $("#btn_play2").click(function () {
-        document.all.bgsound_wav.src = "voice_1.wav";
-    });
-    $("#bt_pause").click(function () {
-        $("#bgsound_wav").attr("src", "");
+
+    //btn_pauseをクリックした時
+    $("#btn_pause").click(function () {
+        $("#audio_ie").attr("src", "");  //bgsoundタグのsrcを空文字に変更すると、音声が停止する。
     });
 });
